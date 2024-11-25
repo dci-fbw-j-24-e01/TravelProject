ffd3c1630a8239c3914d9bddcbc2cdad2341d9a0
dci-student <robin.hagolani@dci-student.org>
2024-11-25 11:55:53 +0100

    init

--- src/main/java/dci/j24e01/TravelBlog/model/Role.java
+++
@@ -1,7 +0,0 @@
-package dci.j24e01.TravelBlog.model;
-
-// Roles for the User entity
-public enum Role {
-    ADMIN,
-    USER
-}
--- src/main/java/dci/j24e01/TravelBlog/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dci.j24e01.TravelBlog.repositories;
-
-import dci.j24e01.TravelBlog.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
