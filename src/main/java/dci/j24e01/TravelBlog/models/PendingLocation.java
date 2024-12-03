7f44d38edfb0bbde4bf30e639c3e93f25773b664
indaleci <indaleci4@gmail.com>
2024-12-03 16:36:21 +0100

    inserted getters and setters on PendingLocation.java

--- src/main/java/dci/j24e01/TravelBlog/models/PendingLocation.java
+++ src/main/java/dci/j24e01/TravelBlog/models/PendingLocation.java
@@ -33,5 +33,76 @@ public class PendingLocation {
         REJECTED
     }
 
-    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(Date visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getSuggestedBy() {
+        return suggestedBy;
+    }
+
+    public void setSuggestedBy(User suggestedBy) {
+        this.suggestedBy = suggestedBy;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
