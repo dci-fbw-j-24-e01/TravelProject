9cff57488710cca47359fc5e6b690c154fd8270d
Herr Oleh Bryl <olegbryl@gmail.com>
2024-12-05 13:52:31 +0100

    add deleteAllByVacationPoint(savedPoint)

--- src/main/java/dci/j24e01/TravelBlog/repositories/PhotoRepository.java
+++ src/main/java/dci/j24e01/TravelBlog/repositories/PhotoRepository.java
@@ -2,7 +2,9 @@ package dci.j24e01.TravelBlog.repositories;
 
 
 import dci.j24e01.TravelBlog.models.Photo;
+import dci.j24e01.TravelBlog.models.VacationPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    void deleteAllByVacationPoint(VacationPoint savedPoint);
 }
--- src/main/java/dci/j24e01/TravelBlog/services/AdminService.java
+++ src/main/java/dci/j24e01/TravelBlog/services/AdminService.java
@@ -125,6 +125,7 @@ public class AdminService {
                 }
             }
             photoRepository.saveAll(photoList);
+            // photoRepository.deleteAllByVacationPoint(savedPoint);
         }
     }
 
